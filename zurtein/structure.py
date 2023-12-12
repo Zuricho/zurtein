68916f7d2b993aec0a91536f525f92c09f0d7368
Zuricho <zbztzhz@163.com>
2023-12-12 16:03:18 +0800

    initialize structure class

---
+++ zurtein/structure.py
@@ -0,0 +1,29 @@
+"""
+Work in progress
+"""
+
+class Structure:
+    def __init__(self, topology: str, coordinates: str):
+        pass
+        
+
+
+
+
+class Topology:
+    def __init__(self, topology: str):
+        pass
+
+
+
+
+
+class Coordinates:
+    def __init__(self, coordinates: str):
+        pass
+
+
+
+
+
+
